d5fe8a460f76bbec4b2f08f0618361502ac32693
Whussz <dragoniiix@mail.ru>
2023-10-15 22:01:19 +0300

    preparation for deployment

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -8,6 +8,10 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import styles from './Navbar.module.scss';
+import { Text, TextTheme } from 'shared/ui/Text/Text';
+import { AppLink } from 'shared/ui/AppLink/AppLink';
+import { RoutePath } from 'shared/config/routerConfig/routerConfig';
+import { AppLinkTheme } from 'shared/config/themeConfig/theme.config';
 
 interface NavbarProps {
 	className?: string;
@@ -37,6 +41,17 @@ export const Navbar = memo(({ className, authData }: NavbarProps) => {
 	if (authData) {
 		return (
 			<header className={classNames(styles.Navbar, {}, [className])}>
+				<Text
+					className={styles.appName}
+					title={t('APP')}
+					theme={TextTheme.INVERTED}
+				/>
+				<AppLink
+					to={RoutePath.article_create}
+					theme={AppLinkTheme.INVERTED}
+				>
+					{t('Создать статью')}
+				</AppLink>
 				<Button
 					theme={ButtonTheme.CLEAR_INVERTED}
 					onClick={onLogout}
